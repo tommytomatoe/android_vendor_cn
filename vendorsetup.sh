76c5a741b16d8e301cd137f02f4eedcf3ab1906c
Tommy Nguyen <tommytomatoe@gmail.com>
2012-02-03 10:28:07 -0500

    removed extra options - leave only supersonic shooter tenderloin

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,3 @@
-add_lunch_combo ev_desirec-eng
-add_lunch_combo ev_inc-eng
-add_lunch_combo ev_mecha-eng
-add_lunch_combo ev_passion-eng
-add_lunch_combo ev_ruby-eng
-add_lunch_combo ev_shooter-eng
-add_lunch_combo ev_speedy-eng
-add_lunch_combo ev_supersonic-eng
-add_lunch_combo ev_tenderloin-eng
-add_lunch_combo ev_thundercls670-eng
-add_lunch_combo ev_vivow-eng
+add_lunch_combo shooter-eng
+add_lunch_combo supersonic-eng
+add_lunch_combo tenderloin-eng
